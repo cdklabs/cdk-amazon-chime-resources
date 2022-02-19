1a81a2c209f70adfb8c88f74e7098202f2db7437
Court Schuett <schuettc@amazon.com>
2022-02-18 18:47:38 -0600

    fix: region update on associate

--- example/bin/cdk-chime-resources-example.ts
+++ example/bin/cdk-chime-resources-example.ts
@@ -3,4 +3,9 @@ import * as cdk from 'aws-cdk-lib';
 import { ChimeExample } from '../lib/cdk-chime-resources-example';
 
 const app = new cdk.App();
-new ChimeExample(app, 'ChimeResources', {});
+new ChimeExample(app, 'ChimeResources', {
+  env: {
+    account: process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEPLOY_REGION || process.env.CDK_DEFAULT_REGION,
+  },
+});
